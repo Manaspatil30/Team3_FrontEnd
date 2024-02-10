0ecc90e23042a27b9f7cb1d654e5e95b8b6a299a
Oguzhan Cetinkaya <cetinnkayaoguzhan@gmail.com>
2024-02-10 16:27:10 +0000

    searchbar

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import Home from './pages/Home';
 import Navbar from './components/Navbar';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Explore from './pages/Explore';
+import Searchbar from './components/Searchbar'
 
 function App() {
+  
   return (
     <div className="app">
       <Navbar />
+      <Searchbar />
       <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home/>}/>

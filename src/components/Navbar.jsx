92444fb0b741820654773ab90ff684d6d62da6b7
Manas Patil <89078720+Manaspatil30@users.noreply.github.com>
2024-02-12 09:08:04 +0000

    added cart icon and login/ signup integration

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
@@ -15,6 +15,11 @@ import Fade from '@mui/material/Fade';
 import Backdrop from '@mui/material/Backdrop';
 import TextField from '@mui/material/TextField';
 import { Link } from '@mui/material';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import Badge from '@mui/material/Badge';
+import axios from 'axios';
+import Cookies from 'js-cookie';
+import { useNavigate } from 'react-router-dom';
 
 const pages = ['Explore', 'Stores', 'Sign in'];
 const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
@@ -33,8 +38,14 @@ const style = {
 };
 
 const Navbar = () => {
-    const [anchorElNav, setAnchorElNav] = React.useState(null);
-  const [anchorElUser, setAnchorElUser] = React.useState(null);
+  const [anchorElNav, setAnchorElNav] = useState(null);
+  const [anchorElUser, setAnchorElUser] = useState(null);
+  const [basketBadge , setBasketBadge] = useState();
+
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+
+  const [login, setlogin] = useState();
 
   const handleOpenNavMenu = (event) => {
     setAnchorElNav(event.currentTarget);
@@ -55,90 +66,126 @@ const Navbar = () => {
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
 
+  useEffect(() => {
+    axios.get('basket/1').then((data) => {
+      setBasketBadge(data.data)
+    })
+  },[])
+
+  console.log('basket badge', basketBadge?.length)
+  const loginData = {
+    "email" : email,
+    "password" : password
+  }
+
+  const userLogin = () => {
+    axios.post('signin', loginData).then((data) => {Cookies.set('jwtToken',data.data.token,{path:'/'})}).then((data) => {Cookies.set('user_id',data.data.userId,{path:'/'})})
+  }
+
+  console.log('Login info', login)
+
+  
+
+  const clearToken = () => {
+    Cookies.remove('jwtToken', {path:'/'})
+  }
+
+  // console.log('basket badge', basketBadge);
+
   return (
     <>
-    <AppBar position="static" sx={{backgroundColor:'#edf3fd', boxShadow:'none'}}>
-      <Container maxWidth="xl">
-        <Toolbar disableGutters>
-          {/* <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} /> */}
-          <Typography
-            variant="h6"
-            noWrap
-            component="a"
-            href="/"
-            sx={{
-              mr: 2,
-              display: { xs: 'none', md: 'flex' },
-              fontFamily: 'monospace',
-              fontWeight: 700,
-              letterSpacing: '.3rem',
-              color: 'inherit',
-              textDecoration: 'none',
-            }}
-          >
-            <Box sx={{padding:'10px'}}>
-            <img src={Logo} height={"50px"} width={"auto"}/>
-            </Box>
-          </Typography>
-
-          <Box sx={{ display: { xs: 'flex', md: 'none' }}}>
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'left',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left',
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
+      <AppBar
+        position="static"
+        sx={{ backgroundColor: "#edf3fd", boxShadow: "none" }}
+      >
+        <Container maxWidth="xl">
+          <Toolbar disableGutters>
+            {/* <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} /> */}
+            <Typography
+              variant="h6"
+              noWrap
+              component="a"
+              href="/"
               sx={{
-                display: { xs: 'block', md: 'none' },
+                mr: 2,
+                display: { xs: "none", md: "flex" },
+                fontFamily: "monospace",
+                fontWeight: 700,
+                letterSpacing: ".3rem",
+                color: "inherit",
+                textDecoration: "none",
               }}
             >
-              {pages.map((page) => (
-                <MenuItem key={page} onClick={handleCloseNavMenu}>
-                  <Typography textAlign="center">{page}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
+              <Box sx={{ padding: "10px" }}>
+                <img src={Logo} height={"50px"} width={"auto"} />
+              </Box>
+            </Typography>
 
-          {/* <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} /> */}
-          <Typography
-            variant="h5"
-            noWrap
-            component="a"
-            href="#app-bar-with-responsive-menu"
-            sx={{
-              mr: 2,
-              display: { xs: 'flex', md: 'none' },
-              flexGrow: 1,
-              fontFamily: 'monospace',
-              fontWeight: 700,
-              letterSpacing: '.3rem',
-              color: 'inherit',
-              textDecoration: 'none',
-            }}
-          >
-            <img src={Logo}/>
-          </Typography>
-          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' }, justifyContent:'end', marginRight:'2rem' }}>
-            {/* {pages.map((page) => (
+            <Box sx={{ display: { xs: "flex", md: "none" } }}>
+              <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleOpenNavMenu}
+                color="inherit"
+              >
+                <MenuIcon />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorElNav}
+                anchorOrigin={{
+                  vertical: "bottom",
+                  horizontal: "left",
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "left",
+                }}
+                open={Boolean(anchorElNav)}
+                onClose={handleCloseNavMenu}
+                sx={{
+                  display: { xs: "block", md: "none" },
+                }}
+              >
+                {pages.map((page) => (
+                  <MenuItem key={page} onClick={handleCloseNavMenu}>
+                    <Typography textAlign="center">{page}</Typography>
+                  </MenuItem>
+                ))}
+              </Menu>
+            </Box>
+
+            {/* <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} /> */}
+            <Typography
+              variant="h5"
+              noWrap
+              component="a"
+              href="#app-bar-with-responsive-menu"
+              sx={{
+                mr: 2,
+                display: { xs: "flex", md: "none" },
+                flexGrow: 1,
+                fontFamily: "monospace",
+                fontWeight: 700,
+                letterSpacing: ".3rem",
+                color: "inherit",
+                textDecoration: "none",
+              }}
+            >
+              <img src={Logo} />
+            </Typography>
+            <Box
+              sx={{
+                flexGrow: 1,
+                display: { xs: "none", md: "flex" },
+                justifyContent: "end",
+                marginRight: "2rem",
+              }}
+            >
+              {/* {pages.map((page) => (
               <Button
                 key={page}
                 onClick={handleCloseNavMenu}
@@ -147,21 +194,55 @@ const Navbar = () => {
                 {page}
               </Button>
             ))} */}
-            <Button
-                href='/explore'
-                sx={{ my: 2, display: 'block', color:'#000', fontWeight:'700' }}
+              <Button
+                href="/explore"
+                sx={{
+                  my: 2,
+                  display: "block",
+                  color: "#000",
+                  fontWeight: "700",
+                }}
               >
                 Explore
+              </Button>
+        {
+          Cookies.get('jwtToken') ? 
+              <Button
+              onClick={()=>{clearToken()}}
+              href='/'
+              sx={{
+                my: 2,
+                display: "block",
+                color: "#000",
+                fontWeight: "700",
+              }}
+            >
+              Logout
             </Button>
-            <Button
+            :
+
+              <Button
                 onClick={handleOpen}
-                sx={{ my: 2, display: 'block', color:'#000', fontWeight:'700' }}
+                sx={{
+                  my: 2,
+                  display: "block",
+                  color: "#000",
+                  fontWeight: "700",
+                }}
               >
                 Sign in
-            </Button>
-          </Box>
+              </Button>
+        }
 
-          {/* <Box sx={{ flexGrow: 0 }}>
+
+              <IconButton href='/cart' sx={{margin:'auto 0'}}>
+              <Badge badgeContent={basketBadge?.length} color="primary">
+                <ShoppingCartIcon color="action" />
+              </Badge>
+              </IconButton>
+            </Box>
+
+            {/* <Box sx={{ flexGrow: 0 }}>
             <Tooltip title="Open settings">
               <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
                 <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
@@ -190,10 +271,10 @@ const Navbar = () => {
               ))}
             </Menu>
           </Box> */}
-        </Toolbar>
-      </Container>
-    </AppBar>
-    <Modal
+          </Toolbar>
+        </Container>
+      </AppBar>
+      <Modal
         aria-labelledby="transition-modal-title"
         aria-describedby="transition-modal-description"
         open={open}
@@ -208,19 +289,33 @@ const Navbar = () => {
       >
         <Fade in={open}>
           <Box sx={style}>
-            <Typography sx={{marginBottom:'20px'}} variant='h3'>Log in</Typography>
-            <Box sx={{marginBottom:'20px'}} >
-              <TextField  fullWidth id="outlined-basic" label="Username" variant="outlined" />
+            <Typography sx={{ marginBottom: "20px" }} variant="h3">
+              Log in
+            </Typography>
+            <Box sx={{ marginBottom: "20px" }}>
+              <TextField
+                fullWidth
+                id="outlined-basic"
+                label="Email"
+                variant="outlined"
+                onChange={(e)=>{setEmail(e.target.value)}}
+              />
             </Box>
-            <Box sx={{marginBottom:'20px'}}>
-              <TextField  fullWidth id="outlined-basic" label="Password" variant="outlined" />
+            <Box sx={{ marginBottom: "20px" }}>
+              <TextField
+                fullWidth
+                id="outlined-basic"
+                label="Password"
+                variant="outlined"
+                onChange={(e)=>{setPassword(e.target.value)}}
+              />
             </Box>
-            <Button sx={{marginBottom:'20px'}} variant='contained'>
+            <Button sx={{ marginBottom: "20px" }} variant="contained" href='/' onClick={()=>{userLogin(); handleClose()}}>
               Log in
             </Button>
             <Box>
-              <Link href='/'>
-              <Typography variant='caption'>new user? Sign up!</Typography>
+              <Link href="/signUp">
+                <Typography variant="caption">new user? Sign up!</Typography>
               </Link>
             </Box>
           </Box>

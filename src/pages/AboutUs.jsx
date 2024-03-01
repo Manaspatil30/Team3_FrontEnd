2ee7bf1506139c5a95d259e4cdcbbc59bd87ced6
uzairamanat <uzair.amanat@hotmail.com>
2024-03-01 11:38:32 +0000

    created About Us page

---
+++ src/pages/AboutUs.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react';
+
+const AboutUs = () => {
+  return (
+    <div className="about-us">
+      <h1>About Us</h1>
+      <p>Welcome to our e-commerce website!</p>
+      <p>Our mission is to provide high-quality products and excellent customer service.</p>
+    </div>
+  );
+}
+
+export default AboutUs;

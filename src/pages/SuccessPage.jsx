e25b40b538b49112f2b7b3e7f788258204d53bf8
Manas Patil <89078720+Manaspatil30@users.noreply.github.com>
2024-03-25 07:54:04 +0000

    success page

--- src/pages/SuccessPage.jsx
+++ src/pages/SuccessPage.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import { Box, CircularProgress, Typography } from '@mui/material'
+import React, { useEffect, useState } from 'react'
+import successimg from '../images/check_mark.png'
 
 const SuccessPage = () => {
+  const [visible, setVisible] = useState(true);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setVisible(false);
+    }, 5000); // Hides the progress bar after 5 seconds
+
+    return () => clearTimeout(timer); // Cleanup function to clear the timeout
+  }, []);
   return (
-    <div>SuccessPage</div>
+    <>
+    <Typography variant='h4' textAlign={'center'} fontWeight={700} mt={5}>Order Placed Successfully!</Typography>
+    <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '70vh' }}>
+      <img width={'600px'} src={successimg}/>
+    </Box>
+    </>
   )
 }
 
